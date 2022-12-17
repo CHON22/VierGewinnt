d31172b0a96254745f4e5e138c91d1c2ee39860c
CHON22 <cel.haupenthal.22@lehre.mosbach.dhbw.de>
2022-12-17 23:39:45 +0100

    Änderung Zeile 62 bis 65

--- VIER_Gewinnt.java
+++ VIER_Gewinnt.java
@@ -59,10 +59,10 @@ public class VIER_Gewinnt {
 
     }
 
-    // Erzeugung eines leeren Spielbretts
+    // Erzeugung des leeren Spielbretts
     // "Geruest" des Spielfelds mit 7 Spalten und 6 Zeilen wird im Rahmen eines
     // zweidimensionalen Arrays erzeugt
-    public static char[][] BoardIsEmpty(char spielfeld[][]) {
+    public static char[][] spielbrettleer(char spielfeld[][]) {
 
         ueberschrift(7);
 
